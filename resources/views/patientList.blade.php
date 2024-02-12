d0c63824fcb2b4b0926f7b26e70f91f1befd9e30
KgySathsara <88894196+KgySathsara@users.noreply.github.com>
2024-02-12 10:52:22 +0530

    patient_list_search function fix

--- resources/views/patientList.blade.php
+++ resources/views/patientList.blade.php
@@ -11,97 +11,65 @@
     </thead>
     <tbody>
         @foreach ($patient_list as $patient)
-            @if (count($appoinment_list) > 0)
+            @php
+                $hourDifference = null; // Initialize before the loop
+                $appointmentPatientIds = [];
+                $appointmentPatienttimes = [];
+                $appoinment_list = [];
+            @endphp
+
+            @if ($appoinment_list && count($appoinment_list) > 0)
                 @php
-                    $hourDifference = null; // Initialize before the loop
+                    $appointmentPatientIds = $appoinment_list->pluck('patient_id')->toArray();
+                    $appointmentPatienttimes = $appoinment_list->pluck('appdateTime')->toArray();
                 @endphp
-
-                @if (count($appoinment_list) > 0)
-                    @php
-                        $appointmentPatientIds = $appoinment_list->pluck('patient_id')->toArray();
-                        $appointmentPatienttimes = $appoinment_list->pluck('appdateTime')->toArray();
-                    @endphp
-
-                    @if (in_array($patient->id, $appointmentPatientIds))
-                        @php
-                            $index = array_search($patient->id, $appointmentPatientIds);
-                            $current = \Carbon\Carbon::now();
-                            $previous = \Carbon\Carbon::Create($appointmentPatienttimes[$index]);
-                            $hourDifference = $previous->diffInHours($current);
-                        @endphp
-                    @endif
-                @endif
-
-                <tr style="font-size: 13px;">
-                    <td>{{ $patient->id }}</td>
-                    <td>{{ $patient->family_name }}</td>
-                    <td>{{ $patient->name }}</td>
-                    <td>{{ $patient->nic }}</td>
-                    <td>{{ $patient->mobile }}</td>
-                    <td class="project-actions">
-                        <center>
-                            @if ($hourDifference < 6 && in_array($patient->id, $appointmentPatientIds))
-                                <!-- Disable the Appointment button -->
-                                <button class="btn btn-outline-dark " disabled>
-                                    <a href="{{ route('appointment.add', $patient->id) }}" title="Appointment"
-                                        class="appointment-link">
-                                        <i class="fa fa-stethoscope"></i>Appointment
-                                    </a>
-                                </button>
-                            @else
-                                <!-- Enable the Appointment button -->
-                                <button class="btn btn-outline-dark" id="appbton">
-                                    <a href="{{ route('appointment.add', $patient->id) }}" title="Appointment"
-                                        class="appointment-link">
-                                        <i class="fa fa-stethoscope"></i>Appointment
-                                    </a>
-                                </button>
-                            @endif
-                            <a href="{{ route('patienteditviewtable', $patient->id) }}" title="Edit">
-                                <button class="btn btn-outline-dark"> <i class="fa fa-edit"></i>Edits</button>
-                            </a>
-                            <a href="{{ route('patientdelete', $patient->id) }}" title="Delete">
-                                <button class="btn btn-outline-danger"> <i class="fa fa-trash"></i>Delete</button>
-                            </a>
-
-
-                            <a href="{{ route('appointment.history', $patient->id) }}" title="History">
-                                <button class="btn btn-outline-info"> <i class="fa fa-book"> </i>History</button>
-                            </a>
-                        </center>
-                    </td>
-                </tr>
-            @else
-                <tr style="font-size: 13px;">
-                    <td>{{ $patient->id }}</td>
-                    <td>{{ $patient->family_name }}</td>
-                    <td>{{ $patient->name }}</td>
-                    <td>{{ $patient->nic }}</td>
-                    <td>{{ $patient->mobile }}</td>
-                    <td class="project-actions ">
-                        <div class="d-flex flex-column align-items-center flex-lg-row gap-2">
-                            <div class="d-flex flex-row gap-2">
-                                <a href="{{ route('appointment.add', $patient->id) }}" title="Appointment">
-                                    <button class="btn btn-outline-dark"> <i
-                                            class="fa fa-stethoscope"></i>Appointment</button>
-                                </a>
-                                <a href="{{ route('patienteditviewtable', $patient->id) }}" title="Edit">
-                                    <button class="btn btn-outline-dark"> <i class="fa fa-edit"></i>Edit</button>
-                                </a>
-
-                            </div>
-                            <div class="d-flex flex-row gap-2">
-                                <a href="{{ route('patientdelete', $patient->id) }}" title="Delete">
-                                    <button class="btn btn-outline-danger"> <i class="fa fa-trash"></i>Delete</button>
-                                </a>
-                                <a href="{{ route('appointment.history', $patient->id) }}" title="History">
-                                    <button class="btn btn-outline-info"> <i class="fa fa-book"> </i>History</button>
-                                </a>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
             @endif
+
+            @if (in_array($patient->id, $appointmentPatientIds))
+                @php
+                    $index = array_search($patient->id, $appointmentPatientIds);
+                    $current = \Carbon\Carbon::now();
+                    $previous = \Carbon\Carbon::Create($appointmentPatienttimes[$index]);
+                    $hourDifference = $previous->diffInHours($current);
+                @endphp
+            @endif
+
+            <tr style="font-size: 13px;">
+                <td>{{ $patient->id }}</td>
+                <td>{{ $patient->family_name }}</td>
+                <td>{{ $patient->name }}</td>
+                <td>{{ $patient->nic }}</td>
+                <td>{{ $patient->mobile }}</td>
+                <td class="project-actions">
+                    <center>
+                        @if ($hourDifference < 6 && in_array($patient->id, $appointmentPatientIds))
+                            <!-- Disable the Appointment button -->
+                            <button class="btn btn-outline-dark" disabled>
+                                <a href="{{ route('appointment.add', $patient->id) }}" title="Appointment" class="appointment-link">
+                                    <i class="fa fa-stethoscope"></i>Appointment
+                                </a>
+                            </button>
+                        @else
+                            <!-- Enable the Appointment button -->
+                            <button class="btn btn-outline-dark appbton">
+                                <a href="{{ route('appointment.add', $patient->id) }}" title="Appointment" class="appointment-link">
+                                    <i class="fa fa-stethoscope"></i>Appointment
+                                </a>
+                            </button>
+                        @endif
+
+                        <a href="{{ route('patienteditviewtable', $patient->id) }}" title="Edit">
+                            <button class="btn btn-outline-dark"> <i class="fa fa-edit"></i>Edits</button>
+                        </a>
+                        <a href="{{ route('patientdelete', $patient->id) }}" title="Delete">
+                            <button class="btn btn-outline-danger"> <i class="fa fa-trash"></i>Delete</button>
+                        </a>
+                        <a href="{{ route('appointment.history', $patient->id) }}" title="History">
+                            <button class="btn btn-outline-info"> <i class="fa fa-book"> </i>History</button>
+                        </a>
+                    </center>
+                </td>
+            </tr>
         @endforeach
     </tbody>
 </table>
